ac5d74af7231033eb1a1ab9316aa10903fc65c6c
BLoved <branlove17@yahoo.com>
2021-11-24 00:58:13 -0500

    add command to log spending

--- Database/Commands/logMoney.js
+++ Database/Commands/logMoney.js
@@ -1,18 +1,19 @@
 const { Ledger } = require('./../Tables/Ledger');
 const { getCurrentWeek } = require('./../../conversions');
 
-const logMoney = async (value, discordAccountId) => {
+const logMoney = async (value, discordAccountId, isSpending = false) => {
     try {
         const log = await Ledger.create({
             week: getCurrentWeek(),
             year: new Date().getFullYear(),
             value: value,
-            discordAccountId: discordAccountId
+            discordAccountId: discordAccountId,
+            valueWasSpent: isSpending
         });
         return log;
     } catch (e) {
         if (e.name === 'SequelizeForeignKeyConstraintError') {
-            throw('that name hasn\'t been added to the Ledger yet, please add with "!addname"');
+            throw('your name hasn\'t been added to the Ledger yet, please add with "!addname"');
         } else {
             throw('something unexpected has happened, please alert bot owner if it happens again');
         }
